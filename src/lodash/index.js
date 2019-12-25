8528683f3520f5346dc77101fa1b8e53b168b0ef
halldwang <email@ahthw.com>
2019-12-25 18:11:58 +0800

    fix: mlodash get fun

--- src/lodash/index.js
+++ src/lodash/index.js
@@ -221,14 +221,42 @@ function toPath(value) {
   return stringToPath(value);
 }
 
-const get = (from, ...selectors) =>
-  [...selectors].map((s) =>
-    s
-      .replace(/\[([^\[\]]*)\]/g, '.$1.')
-      .split('.')
-      .filter((t) => t !== '')
-      .reduce((prev, cur) => prev && prev[cur], from)
-  );
+function get(object, path, defaultValue) {
+  if (object == null) {
+    return defaultValue;
+  }
+  if (!Array.isArray(path)) {
+    const reIsDeepProp = /\.|\[(?:[^[\]]*|(["'])(?:(?!\1)[^\\]|\\.)*?\1)\]/;
+    const reIsPlainProp = /^\w*$/;
+    const isKey = function(value, object) {
+      const type = typeof value;
+      if (type == 'number' || type == 'boolean' || value == null) {
+        return true;
+      }
+      return (
+        reIsPlainProp.test(value) ||
+        !reIsDeepProp.test(value) ||
+        (object != null && value in Object(object))
+      );
+    };
+    if (isKey(path, object)) {
+      path = [path];
+    } else {
+      path = stringToPath(path);
+    }
+  }
+  let index = 0;
+  const length = path.length;
+  while (object != null && index < length) {
+    object = object[path[index]];
+    index += 1;
+  }
+  if (index && index === length) {
+    return object === undefined ? defaultValue : object;
+  } else {
+    return defaultValue;
+  }
+}
 
 function debounce(func, wait, options) {
   let lastArgs, lastThis, maxWait, result, timerId, lastCallTime;
