74440fd326ec4136684bb42c8aa58e318a7dd967
halldwang <ahthw@hotmail.com>
2020-01-21 16:07:43 +0800

    docs: for containsWith doc

--- src/string/contains_with.js
+++ src/string/contains_with.js
@@ -4,7 +4,7 @@
 export default function containsWith(target, item) {
   // discuss at: https://locutus.io/golang/strings/Contains
   // original by: Kevin van Zonneveld (https://kvz.io)
-  // example 1: bbo.string.contains('Kevin', 'K')
+  // example 1: bbo.contains('Kevin', 'K')
   // returns 1: true
   return String(target).indexOf(item) !== -1;
 }
