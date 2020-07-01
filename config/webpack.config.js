fe8cefe1124d3964787684b7c850c7d56694377f
halldwang <email@ahthw.com>
2020-07-01 11:55:40 +0800

    feat: devtool

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
   mode: 'development',
   entry: {
@@ -24,7 +23,6 @@ module.exports = {
   ],
   devtool: 'source-map',
   devServer: {
-    host: '0.0.0.0',
     disableHostCheck: true
   }
 };
