5da08969c3af90a71c13577412ad79946af153aa
halldwang <email@ahthw.com>
2020-07-01 11:08:16 +0800

    fix: object-reduce

--- src/lodash/reduce.js
+++ src/lodash/reduce.js
@@ -1,29 +1,18 @@
-import isArray from './is_array';
-import isObject from './is_object';
+/* eslint-disable no-param-reassign */
+/* eslint-disable max-params */
+export default function reduce(obj, predicate /* , initialValue */) {
+  let args = [callback];
 
-export default function reduce(src, func) {
-  let i = 0;
-  let acc = arguments[2];
-  if (isArray(src)) {
-    if (arguments.length !== 3) {
-      acc = src[0];
+  let hasInitialValue = 2 in arguments;
+  hasInitialValue && args.push(arguments[2]);
+
+  function callback(previousValue, currentKey, currentIndex, array) {
+    if (!hasInitialValue) {
+      previousValue = obj[array[0]];
+      hasInitialValue = true;
     }
-    while (i < src.length) {
-      acc = func(acc, src[i], i, src);
-      i += 1;
-    }
-    return acc;
-  } else if (isObject(src)) {
-    const keys = Object.keys(src);
-    if (arguments.length !== 3) {
-      acc = src[keys[0]];
-    }
-    while (i < keys.length) {
-      const key = keys[i];
-      acc = func(acc, src[key], key, src);
-      i += 1;
-    }
-    return acc;
+    return predicate(previousValue, currentKey, obj[currentKey], currentIndex, array);
   }
-  return acc;
+
+  return Array.prototype.reduce.apply(Object.keys(obj), args);
 }
