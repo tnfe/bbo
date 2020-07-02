b1926cbcb2985ae1194070d5457eef8eabceccae
halldwang <email@ahthw.com>
2020-07-02 20:16:01 +0800

    fix: dasherize

--- src/string/dasherize.js
+++ src/string/dasherize.js
@@ -3,5 +3,5 @@ import underscored from './underscored';
  * Turn '_' in a string into '-'
  */
 export default function dasherize(target) {
-  return underscored(target).replace(/_/g, '-');
+  return underscored(String(target)).replace(/_/g, '-');
 }
