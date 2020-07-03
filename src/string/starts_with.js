e82ec6a2e9227fa6d61ab3274a6117211d771da1
halldwang <email@ahthw.com>
2020-07-03 17:24:36 +0800

    fix: string startsWith

--- src/string/starts_with.js
+++ src/string/starts_with.js
@@ -2,6 +2,6 @@
  *  Item is the beginning of the target
  */
 export default function startsWith(target, item, ignore) {
-  let str = target.slice(0, item.length);
+  let str = String(target).slice(0, item.length);
   return ignore ? str.toLowerCase() === item.toLowerCase() : str === item;
 }
