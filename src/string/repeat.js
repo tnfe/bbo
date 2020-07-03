b92c21af098c222ba54f5864e9da2920bed43a3d
halldwang <email@ahthw.com>
2020-07-03 16:13:23 +0800

    feat: string repeat

--- src/string/repeat.js
+++ src/string/repeat.js
@@ -2,7 +2,7 @@
  * Repeat item, times times
  */
 export default function repeat(item, times) {
-  let s = item;
+  let s = String(item);
   let target = '';
   while (times > 0) {
     if (times % 2 === 1) {
