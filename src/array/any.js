016bbc26bcfecb1b4a66a0b0f1b224f94165c931
halldwang <email@ahthw.com>
2020-07-09 10:04:47 +0800

    docs: array(any)

--- src/array/any.js
+++ src/array/any.js
@@ -1,6 +1,5 @@
 /**
- * Returns true if the provided predicate function returns true for at least one element in a collection,
- * false otherwise.
+ * Returns true if the provided predicate function returns true for at least one element in a collection,false otherwise.
  */
 const any = (arr, fn = Boolean) => arr.some(fn);
 
