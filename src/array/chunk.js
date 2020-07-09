be5246ca7dcda88a7ccd7c6f8542055c770eface
halldwang <email@ahthw.com>
2020-07-09 10:28:53 +0800

    feat: array(chunk)

--- src/array/chunk.js
+++ src/array/chunk.js
@@ -2,7 +2,7 @@
  * Chunks an array into smaller arrays of a specified size.
  */
 const chunk = (arr, size) => {
-  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+  return Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
     arr.slice(i * size, i * size + size)
   );
 };
