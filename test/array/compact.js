396ced91a23082c7c99d0136e8ff2908131592d8
halldwang <email@ahthw.com>
2020-07-10 18:05:26 +0800

    test: array(compact) update

--- test/array/compact.js
+++ test/array/compact.js
@@ -7,6 +7,7 @@ describe('compact', () => {
     const array = [1, 2, 3, 4, 5];
     expect(bbo.compact([1, null, 2, undefined, null, NaN, 3, 4, false, 5], 3)).toEqual(array);
 
+    expect(bbo.compact([1, 7, undefined, null, '', 0, false])).toEqual([1, 7]);
     expect(bbo.compact([1, 2, [], 4, {}])).toEqual([1, 2, [], 4, {}]);
     expect(bbo.compact([])).toEqual([]);
   });
