95295cc18606e33a222a86df5ca6b8661b4278f8
halldwang <email@ahthw.com>
2020-07-11 22:23:02 +0800

    feat: array(countby)

--- src/array/count_by.js
+++ src/array/count_by.js
@@ -2,7 +2,7 @@
  * Groups the elements of an array based on the given function and returns the count of elements in each group.
  */
 const countBy = (arr, fn) => {
-  arr.map(typeof fn === 'function' ? fn : (val) => val[fn]).reduce((acc, val) => {
+  return arr.map(typeof fn === 'function' ? fn : (val) => val[fn]).reduce((acc, val) => {
     acc[val] = (acc[val] || 0) + 1;
     return acc;
   }, {});
