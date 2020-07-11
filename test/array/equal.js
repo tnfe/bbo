702ce513849411064d7a27f398f57e2cfc61d5c5
halldwang <email@ahthw.com>
2020-07-11 23:28:56 +0800

    test: array(equal)

---
+++ test/array/equal.js
@@ -0,0 +1,9 @@
+import bbo from '../bbo';
+
+describe('equal', () => {
+  it('base case', () => {
+    expect(bbo.equal([1, 2, 3], [1, 2, 3])).toBe(true);
+    expect(bbo.equal([1, 2, 3], [1, 2, 4])).toBe(false);
+    expect(bbo.equal([], [])).toBe(true);
+  });
+});
