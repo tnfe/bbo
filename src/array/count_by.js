063760e0b9f7033f04158c2ad0691c2499ddcf14
halldwang <email@ahthw.com>
2020-07-11 22:43:37 +0800

    feat: array(countBy)

--- src/array/count_by.js
+++ src/array/count_by.js
@@ -1,8 +1,11 @@
 /**
  * Groups the elements of an array based on the given function and returns the count of elements in each group.
  */
+
+import isFunction from '../lodash/is_function';
+
 const countBy = (arr, fn) => {
-  return arr.map(typeof fn === 'function' ? fn : (val) => val[fn]).reduce((acc, val) => {
+  return arr.map(isFunction(fn) ? fn : (val) => val[fn]).reduce((acc, val) => {
     acc[val] = (acc[val] || 0) + 1;
     return acc;
   }, {});
