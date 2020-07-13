34245f13e9bd1d90eda78773c46a03fd7476fabc
halldwang <email@ahthw.com>
2020-07-13 18:17:02 +0800

    test: array(split)

---
+++ test/array/split.js
@@ -0,0 +1,10 @@
+import bbo from '../bbo';
+
+describe('split', () => {
+  const array = [1, 2, 3, 4, 5];
+
+  it('base case 1', () => {
+    expect(bbo.split(array, 2)).toEqual([[1, 2], [3, 4], [5]]);
+    expect(bbo.split([], 3)).toEqual([]);
+  });
+});
