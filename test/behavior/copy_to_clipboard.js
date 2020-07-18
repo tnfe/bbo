2ebed32d8bbca6ebe5fec1aba8c9abd2dcdbef71
undefined <undefined>
2020-07-18 14:27:30 +0800

    test: behavior(lockTouch)

--- test/behavior/copy_to_clipboard.js
+++ test/behavior/copy_to_clipboard.js
@@ -1,44 +1,46 @@
 import bbo from '../bbo';
 
-test('copyToClipboard is a Function', () => {
-  expect(bbo.copyToClipboard).toBeInstanceOf(Function);
-});
-test('copyToClipboard does not throw errors', () => {
-  document.getSelection = function() {
-    return {
-      rangeCount: 0,
-      removeAllRanges() {
-        return;
-      },
-      addRange(x) {
-        return x;
-      }
+describe('copyToClipboard', () => {
+  test('copyToClipboard is a Function', () => {
+    expect(bbo.copyToClipboard).toBeInstanceOf(Function);
+  });
+  test('copyToClipboard does not throw errors', () => {
+    document.getSelection = function() {
+      return {
+        rangeCount: 0,
+        removeAllRanges() {
+          return;
+        },
+        addRange(x) {
+          return x;
+        }
+      };
     };
-  };
-  document.execCommand = function(x) {
-    return x;
-  };
-
-  expect(bbo.copyToClipboard('hi')).toBe(undefined);
-});
-test('copyToClipboard does not throw errors', () => {
-  document.getSelection = function() {
-    return {
-      rangeCount: 1,
-      getRangeAt(x) {
-        return x + 1;
-      },
-      removeAllRanges() {
-        return;
-      },
-      addRange(x) {
-        return x;
-      }
+    document.execCommand = function(x) {
+      return x;
     };
-  };
-  document.execCommand = function(x) {
-    return x;
-  };
 
-  expect(bbo.copyToClipboard('hi')).toBe(undefined);
+    expect(bbo.copyToClipboard('hi')).toBe(undefined);
+  });
+  test('copyToClipboard does not throw errors', () => {
+    document.getSelection = function() {
+      return {
+        rangeCount: 1,
+        getRangeAt(x) {
+          return x + 1;
+        },
+        removeAllRanges() {
+          return;
+        },
+        addRange(x) {
+          return x;
+        }
+      };
+    };
+    document.execCommand = function(x) {
+      return x;
+    };
+
+    expect(bbo.copyToClipboard('hi')).toBe(undefined);
+  });
 });
---
+++ test/behavior/lock_touch.js
@@ -0,0 +1,33 @@
+import bbo from '../bbo';
+
+describe('lockTouch', () => {
+  it('should pass example 1', () => {
+    const button = document.createElement('button');
+    button.innerText = 'click me';
+    document.body.appendChild(button);
+    const buttons = document.querySelectorAll('button');
+
+    expect(buttons).toHaveLength(1); // true
+    expect(buttons[0].innerText).toBe('click me'); // true
+
+    buttons[0].addEventListener('touchmove', (e) => {
+      e.target.innerText = 'touchmoved';
+    });
+
+    buttons[0].addEventListener('touchstart', (e) => {
+      e.target.innerText = 'touchstartd';
+    });
+    expect(buttons[0].innerText).not.toBe('touchmoved'); //true
+
+    const touchmove = new Event('touchmove');
+    buttons[0].dispatchEvent(touchmove);
+    expect(buttons[0].innerText).toBe('touchmoved'); // true
+
+    bbo.lockTouch();
+
+    const touchstart = new Event('touchstart');
+    buttons[0].dispatchEvent(touchstart);
+
+    expect(buttons[0].innerText).toBe('touchstartd'); // true
+  });
+});
