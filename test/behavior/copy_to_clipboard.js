fee427564377f00ad292d712ba836211288a189d
undefined <undefined>
2020-07-18 13:09:24 +0800

    test: behavior(copyToClipboard)

---
+++ test/behavior/copy_to_clipboard.js
@@ -0,0 +1,44 @@
+import bbo from '../bbo';
+
+test('copyToClipboard is a Function', () => {
+  expect(bbo.copyToClipboard).toBeInstanceOf(Function);
+});
+test('copyToClipboard does not throw errors', () => {
+  document.getSelection = function() {
+    return {
+      rangeCount: 0,
+      removeAllRanges() {
+        return;
+      },
+      addRange(x) {
+        return x;
+      }
+    };
+  };
+  document.execCommand = function(x) {
+    return x;
+  };
+
+  expect(bbo.copyToClipboard('hi')).toBe(undefined);
+});
+test('copyToClipboard does not throw errors', () => {
+  document.getSelection = function() {
+    return {
+      rangeCount: 1,
+      getRangeAt(x) {
+        return x + 1;
+      },
+      removeAllRanges() {
+        return;
+      },
+      addRange(x) {
+        return x;
+      }
+    };
+  };
+  document.execCommand = function(x) {
+    return x;
+  };
+
+  expect(bbo.copyToClipboard('hi')).toBe(undefined);
+});
