7f33278aa6d48507f8861bcc4b8728e89871dd5b
halldwang <email@ahthw.com>
2020-07-19 17:05:40 +0800

    test: collection(search)

--- test/collection/search.js
+++ test/collection/search.js
@@ -16,4 +16,20 @@ describe('search', () => {
     let result = bbo.search('3', { a: 3, b: 5, c: 7 });
     expect(result).toEqual(expected);
   });
+
+  it('should pass example 3', () => {
+    let expected = 'a';
+    let result = bbo.search('3', { a: 3, b: 5, c: 7 });
+    expect(result).toEqual(expected);
+    expect(bbo.search('3', { a: 3, b: 5, c: 7 }, 0)).toEqual(expected);
+  });
+
+  it('should pass example 3', () => {
+    expect(bbo.search({})).toEqual(false);
+    expect(bbo.search([])).toEqual(false);
+    expect(bbo.search(null)).toEqual(false);
+    expect(bbo.search('1')).toEqual(false);
+    expect(bbo.search(() => {})).toEqual(false);
+    expect(bbo.search({}, {}, {})).toEqual(false);
+  });
 });
