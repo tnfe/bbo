001566bf42fdf476d2f78269da5756d6a8f09eb8
halldwang <email@ahthw.com>
2020-07-20 23:42:39 +0800

    test: string(camelize)

--- test/string/camelize.js
+++ test/string/camelize.js
@@ -4,4 +4,7 @@ describe('camelize', function() {
   test('_ or - to camelCase', function() {
     expect(bbo.camelize('to-upper-case')).toStrictEqual('toUpperCase');
   });
+  test('base case', function() {
+    expect(bbo.camelize('toUpperCase')).toStrictEqual('toUpperCase');
+  });
 });
