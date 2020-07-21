aeaddd79506aff0cda19cd843f50f5fff4a4c0d0
halldwang <email@ahthw.com>
2020-07-21 13:18:03 +0800

    test: feat isEmpty

--- test/lodash/is_empty.js
+++ test/lodash/is_empty.js
@@ -1,42 +1,39 @@
 import bbo from '../bbo';
 
-describe('empty', function() {
+describe('isEmpty', function() {
   it('empty object, array, map or set', function() {
     expect(bbo.isEmpty({})).toBe(true);
     expect(bbo.isEmpty([])).toBe(true);
-    expect(bbo.isEmpty(new Set())).toBe(true);
-    expect(bbo.isEmpty(new Map())).toBe(true);
+    expect(typeof Set === 'function' ? bbo.isEmpty(new Set()) : true).toBe(true);
+    expect(typeof Map === 'function' ? bbo.isEmpty(new Map()) : true).toBe(true);
   });
-});
 
-describe('non-empty object', function() {
   it('non-empty object, array, map or set', function() {
     expect(bbo.isEmpty({ a: 3, b: 5 })).toBe(false);
     expect(bbo.isEmpty([1, 2])).toBe(false);
     expect(bbo.isEmpty(['a', 'b'])).toBe(false);
     expect(bbo.isEmpty(new Array(4))).toBe(false);
-    // expect(bbo.isEmpty(new Set([1, 2, 2]))).toBe(false);
-    // expect(bbo.isEmpty(new Map().set('a', 2))).toBe(false);
-  });
-});
+    expect(
+      bbo.isEmpty(typeof Set === 'function' ? bbo.isEmpty(new Set([1, 2, 2])) : true)
+    ).not.toBe(false);
 
-describe('null undefined', function() {
+    expect(
+      bbo.isEmpty(typeof Map === 'function' ? bbo.isEmpty(new Map().set('a', 2)) : true)
+    ).not.toBe(false);
+  });
+
   it('null or undefined', function() {
     expect(bbo.isEmpty(null)).toBe(true);
     expect(bbo.isEmpty(undefined)).toBe(true);
   });
-});
 
-describe('other', function() {
   it('other primitives', function() {
     expect(bbo.isEmpty(true)).toBe(true);
     expect(bbo.isEmpty(false)).toBe(true);
     expect(bbo.isEmpty('hello')).toBe(false);
     expect(bbo.isEmpty('')).toBe(true);
   });
-});
 
-describe('other object types', function() {
   it('other primitives', function() {
     expect(bbo.isEmpty(/^abc$/)).toBe(true);
     expect(bbo.isEmpty(0)).toBe(true);
@@ -44,5 +41,16 @@ describe('other object types', function() {
     expect(bbo.isEmpty('abc')).toBe(false);
     // eslint-disable-next-line no-new-wrappers
     expect(bbo.isEmpty(new Boolean(true))).toBe(true);
+
+    const m = new Map([
+      [1, 'a'],
+      [2, 'b']
+    ]);
+    expect(bbo.isEmpty(m)).toBe(true);
+
+    const set = new Set([1, 2, 3, 4, 5, 6, 5]);
+    expect(bbo.isEmpty(set)).toBe(true);
+
+    expect(bbo.isEmpty(undefined)).toBe(true);
   });
 });
