ffd8499c776e9e8d0f7d58c22b31fc91ac88786e
hrout <email@ahthw.com>
2020-08-09 15:44:00 +0800

    test: device (is_android)

--- test/device/is_android.js
+++ test/device/is_android.js
@@ -1,7 +1,13 @@
 import bbo from '../bbo';
+import { android, chrome, edge } from '../const';
+
 describe('isAndroid', () => {
   // ua = navigator.userAgent
   // jest jsdom ua = "Mozilla/5.0 (darwin) AppleWebKit/537.36 (KHTML, like Gecko) jsdom/11.12.0"
+
+  const isAndroid = (ua) => {
+    return ua.toLowerCase().indexOf('android') > -1;
+  };
 
   test('bbo.isAndroid is a Function', () => {
     expect(bbo.isAndroid).toBeInstanceOf(Function);
@@ -9,5 +15,8 @@ describe('isAndroid', () => {
 
   test('bbo.isAndroid() for ua', () => {
     expect(bbo.isAndroid()).toBe(false);
+    expect(isAndroid(android)).toBe(true);
+    expect(isAndroid(chrome)).toBe(false);
+    expect(isAndroid(edge)).toBe(false);
   });
 });
