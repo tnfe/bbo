6ab96c8cef67791072796c521eec9c616566e8c5
hrout <email@ahthw.com>
2020-08-13 13:37:20 +0800

    test: json(toJson)

---
+++ test/json/to_json.js
@@ -0,0 +1,24 @@
+import bbo from '../bbo';
+
+describe('toJson', () => {
+  test('bbo.toJson() is a Function', () => {
+    expect(bbo.toJson).toBeInstanceOf(Function);
+  });
+
+  test('should pass example', () => {
+    const a = '{"a":"[Function function () {}]"}';
+    const b = '{"a":1,"b":"[Circular ~]"}';
+    const res = "{ code: 0 , msg: 'msg' , data: {} }";
+
+    expect(bbo.toJson(a)).toEqual({
+      a: '[Function function () {}]'
+    });
+
+    expect(bbo.toJson(b)).toEqual({
+      a: 1,
+      b: '[Circular ~]'
+    });
+
+    expect(bbo.toJson(res)).toEqual({ code: 0, msg: 'msg', data: {} });
+  });
+});
