465b8861dbe8db9026fe1efc3a2264cd12971758
hrout <email@ahthw.com>
2020-08-24 09:47:16 +0800

    feat: collection(search)

--- src/collection/search.js
+++ src/collection/search.js
@@ -1,34 +1,10 @@
-import isFunction from '../lodash/is_function';
+import hasOwnProperty from '../args/has_own_property';
 
 export default function search(needle, haystack, argStrict) {
   let strict = !!argStrict;
   let key = '';
-  let _needle = needle;
-
-  if (isFunction(_needle) && _needle.exec) {
-    // Duck-type for RegExp
-    if (!strict) {
-      // Let's consider case sensitive searches as strict
-      let flags =
-        'i' +
-        (_needle.global ? 'g' : '') +
-        (_needle.multiline ? 'm' : '') +
-        // sticky is FF only
-        (_needle.sticky ? 'y' : '');
-      _needle = new RegExp(_needle.source, flags);
-    }
-    for (key in haystack) {
-      if (haystack.hasOwnProperty(key)) {
-        if (_needle.test(haystack[key])) {
-          return key;
-        }
-      }
-    }
-    return false;
-  }
-
   for (key in haystack) {
-    if (haystack.hasOwnProperty(key)) {
+    if (hasOwnProperty(haystack, key)) {
       // eslint-disable-next-line eqeqeq
       if ((strict && haystack[key] === needle) || (!strict && haystack[key] == needle)) {
         return key;
