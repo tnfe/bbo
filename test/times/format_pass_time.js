4361d95b4ac0f8cda884fdbcb1cfb257ef4914e2
hrout <email@ahthw.com>
2020-08-25 14:19:17 +0800

    test: time(format_pass_time)

--- test/times/format_pass_time.js
+++ test/times/format_pass_time.js
@@ -6,8 +6,12 @@ describe('formatPassTime', function() {
   });
   test('date aogo', () => {
     const aDay = 24 * 60 * 60 * 1000;
+    expect(bbo.formatPassTime(new Date(Date.now() - 60 * 30))).toBe('1 seconds');
+    expect(bbo.formatPassTime(new Date(Date.now() - 0.5 * 60 * 60 * 1000))).toBe('30 minutes');
+    expect(bbo.formatPassTime(new Date(Date.now() - 2 * 60 * 60 * 1000))).toBe('2 hours');
     expect(bbo.formatPassTime(new Date(Date.now() - aDay))).toBe('24 hours');
     expect(bbo.formatPassTime(new Date(Date.now() - aDay * 2))).toBe('2 days');
+    expect(bbo.formatPassTime(new Date(Date.now() - aDay * 33))).toBe('1 months');
     expect(bbo.formatPassTime(new Date(Date.now() - aDay * 365 * 50))).toBe('50 years');
   });
 });
