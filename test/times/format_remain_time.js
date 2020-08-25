6d21d1ab28d9eac9607e255fa2bc1bc0cf8e5cb4
hrout <email@ahthw.com>
2020-08-25 15:19:49 +0800

    test: time(format_remain_time)

---
+++ test/times/format_remain_time.js
@@ -0,0 +1,16 @@
+import bbo from '../bbo';
+
+describe('formatRemainTime', function() {
+  test('formatRemainTime is a Function', () => {
+    expect(bbo.formatRemainTime).toBeInstanceOf(Function);
+  });
+  test('eemain time', () => {
+    const aDay = 24 * 60 * 60 * 1000;
+    const aHour = 60 * 60 * 1000;
+    const aMinute = 60 * 1000;
+    const aSecond = 1000;
+    const endTime = Date.now() + aDay + aHour + aMinute + aSecond;
+
+    expect(bbo.formatRemainTime(endTime)).toBe('1day 1hour 1minute 1second');
+  });
+});
